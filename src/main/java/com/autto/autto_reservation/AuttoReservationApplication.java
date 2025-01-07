b81b5b19258a48f2e76c7c67102f5b47291b285d
angierkd <everydayangie.k@gmail.com>
2025-01-07 11:16:19 +0900

    feat: Initial commit

---
+++ src/main/java/com/autto/autto_reservation/AuttoReservationApplication.java
@@ -0,0 +1,13 @@
+package com.autto.autto_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuttoReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuttoReservationApplication.class, args);
+	}
+
+}
